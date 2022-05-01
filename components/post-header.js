736eaf60163ab4ab932de12137b29aa588b6c92c
Carolina <ccobo.dev@gmail.com>
2022-05-01 14:58:17 +0100

    more styles

--- components/post-header.js
+++ components/post-header.js
@@ -3,17 +3,17 @@ import CoverImage from "../components/cover-image";
 export default function PostHeader({ title, coverImage, links, ctaLabel }) {
   return (
     <div className="container mx-auto ">
-      <div className="mt-10 lg:grid lg:grid-cols-6 lg:gap-6 pb-6 flex flex-col-reverse">
+      <div className="mt-10 md:grid sm:grid-cols-6 sm:gap-6 pb-6 flex flex-col-reverse">
         <div className="md:col-start-2 md:col-span-2 md:place-content-center">
           <h1 className="mb-6 pt-16 text-3xl tracking-tight font-medium text-white sm:text4xl md:text-5xl ">
             {title}
           </h1>
-          <div className="sm:w-3/4">
+          <div className="md:w-3/4">
             {links &&
               links.map(({ label, url }) => (
                 <div key={url} className="mt-8">
                   <a
-                    className="block mb-8 py-3 border md:text-sm sm:mb-0 md:mr-2 px-8 mx-auto text-center bg-white hover:bg-gradient-to-r hover:from-sky-600 hover:to-sky-500 hover:text-white"
+                    className="block mb-8 py-3 border md:text-sm sm:mb-0 md:mr-2 px-8 mx-auto text-center bg-white hover:bg-gradient-to-r hover:from-sky-600 hover:to-sky-500 hover:text-white "
                     href={url}
                   >
                     {label}
@@ -22,7 +22,7 @@ export default function PostHeader({ title, coverImage, links, ctaLabel }) {
               ))}
           </div>
         </div>
-        <div className="md:col-span-2">
+        <div className="md:col-span-2 md:mt-36">
           {coverImage && (
             <CoverImage
               title={title}
