cb066d8f48e04355c0de87468d97b4eb1129801c
Carolina <ccobo.dev@gmail.com>
2022-05-04 08:11:50 +0100

    height

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div className="lg:py-64 w-56 pb-11 md:py-16 pl-8 text-center bg-center md:mx-auto md:w-full md:pt-16 md:pb-20 lg:text-center lg:max-w-7xl">
+    <div className="2xl:py-80 w-56 pb-11 lg:py-16 pl-8 text-center bg-center md:mx-auto md:w-full md:pt-16 md:pb-20 lg:text-center lg:max-w-7xl">
       <div className="md:text-center bg-gray-900 pb-2 md:px-6 bg-opacity-25 md:w-1/2 sm:px-8 text-background lg:py-10">
         <h1 className="text-xl tracking-tight font-extrabold text-white md:text-6xl">
           <span className="">Hi, I&apos;m </span>{" "}
