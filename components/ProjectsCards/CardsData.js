5d161214896a03b65b6e6405c79e31bdd0ad351d
Carolina Cobo <58437550+CarolinaCobo@users.noreply.github.com>
2022-07-13 20:38:53 +0200

    Update CardsData.js

--- components/ProjectsCards/CardsData.js
+++ components/ProjectsCards/CardsData.js
@@ -31,12 +31,20 @@ const projectsData = [
       "https://jesuscoboportfolio.s3.amazonaws.com/Job+Aid+-+Back+Safety+for+Movers.pdf",
   },
   {
+    name: "How to File an Insurance Claim",
+    description: "VIDEO | VYOND",
+    href: "/projects/using-powerpoint-slide-master",
+    imageUrl: "/powerpoint-slide.png",
+    projectHref:
+      "https://jesuscoboportfolio.s3.amazonaws.com/Quick+Tips+PowerPoint+Slide+Master.mp4",
+  },
+  {
     name: "Using PowerPoint Slide Master",
     description: "VIDEO TUTORIAL | CAMTASIA",
     href: "/projects/using-powerpoint-slide-master",
     imageUrl: "/powerpoint-slide.png",
     projectHref:
-      "https://jesuscoboportfolio.s3.amazonaws.com/Quick+Tips+PowerPoint+Slide+Master.mp4",
+      "https://jesuscoboportfolio.s3.amazonaws.com/Filing+An+Insurance+Claim-1080p-220713.mp4",
   },
 ];
 
