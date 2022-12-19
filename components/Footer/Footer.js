f5ca4152273831a17c4f5a32147a07abff02b2c5
Carolina Cobo <58437550+CarolinaCobo@users.noreply.github.com>
2022-12-19 21:25:01 +0000

    Update year

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -49,7 +49,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:mt-0 md:order-1">
           <p className="text-center text-base text-gray-900">
-            &copy; 2022 Jesús Cobo. All rights reserved.
+           &copy; {new Date().getFullYear()} Jesús Cobo. All rights reserved.
           </p>
         </div>
       </div>
